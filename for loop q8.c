3d1cb37d1cc122c15c395c41085857b78cfce03c
EliteCoder457 <codercm123@gmail.com>
2025-11-01 19:03:53 +0530

    Create for loop q8.c

---
+++ for loop q8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b, result = 1;
+    printf("Enter base (a): ");
+    scanf("%d", &a);
+    printf("Enter exponent (b): ");
+    scanf("%d", &b);
+
+    for (int i = 1; i <= b; i++) {
+        result *= a;
+    }
+    printf("%d^%d = %d", a, b, result);
+    return 0;
+}
